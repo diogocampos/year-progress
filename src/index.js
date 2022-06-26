e36acc90992e0a4d13238e770cfd438942088516
Diogo Campos <diogocampos@users.noreply.github.com>
2022-06-26 18:36:16 -0300

    refactor: code style improvements

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
+const UPDATES_PER_SECOND = 30
+
 document.addEventListener('DOMContentLoaded', () => {
-  const title = $('#title')
-  const bar = $('#progress-bar')
-  const percentage = $('#progress-percentage')
+  const $title = $('#title')
+  const $bar = $('#progress-bar')
+  const $percentage = $('#progress-percentage')
 
   setInterval(() => {
-    title.innerText = new Date().getFullYear()
-  }, 1000)
-
-  setInterval(() => {
-    const progress = 100 * yearProgress()
-    bar.style.setProperty('width', `${progress}%`)
-    percentage.innerText = progress.toFixed(10)
-  }, 1000 / 30)
+    const { year, percentage } = getYearProgress()
+    $title.innerText = year
+    $bar.style.setProperty('width', `${percentage}%`)
+    $percentage.innerText = percentage.toFixed(10)
+  }, 1000 / UPDATES_PER_SECOND)
 })
+
+// Helpers
 
 const $ = (selector) => document.querySelector(selector)
 
-function yearProgress() {
+function getYearProgress() {
   const year = new Date().getFullYear()
   const start = new Date(`${year}`)
   const end = new Date(`${year + 1}`)
-  return (Date.now() - start) / (end - start)
+  const percentage = 100 * ((Date.now() - start) / (end - start))
+  return { year, percentage }
 }
